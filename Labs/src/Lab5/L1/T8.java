624131bbf348773bfe59975dedeaa8e10c44bf6c
DanilPodpalyi <danil.podpalyi@gmail.com>
2021-10-28 15:57:50 +0300

    11

--- Labs/src/Lab5/L1/T8.java
+++ Labs/src/Lab5/L1/T8.java
@@ -1,8 +1,19 @@
 package Lab5.L1;
 
+import java.util.Scanner;
+
 public class T8 {
     public static void main(String[] args){
-        System.out.print("Yes");
+        System.out.print("Введите первую строку: ");
+        Scanner console = new Scanner (System.in);
+        String s = console.next();
+        System.out.print("Введите вторую строку: ");
+        String t = console.next();
+        String reverse = "";
+        for ( int i = t.length() - 1;i>=t.length()-s.length() ; i-- ) {
+            reverse = t.charAt(i)+reverse ;
+        }
+        System.out.println(s.equals(reverse));
     }
 
 }
