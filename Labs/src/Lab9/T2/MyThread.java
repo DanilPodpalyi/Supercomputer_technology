113a1372fd2f26971c37f83d6969a6df0c467bbf
DanilPodpalyi <danil.podpalyi@gmail.com>
2021-12-10 02:33:21 +0300

    Press F

---
+++ Labs/src/Lab9/T2/MyThread.java
@@ -0,0 +1,24 @@
+package Lab9.T2;
+
+public class MyThread extends Thread {
+    StringBuilder a;
+
+    public MyThread(StringBuilder a) {
+        this.a = a;
+    }
+
+    public void run() {
+        synchronized (a) {
+            for (int i = 0; i < 100; ++i)
+                System.out.print(i + "" + a + "    ");
+            System.out.println("\n");
+            a.setCharAt(0, (char) (a.charAt(0) + 1));
+        }
+    }
+
+    public static void main(String[] msi) {
+        StringBuilder s = new StringBuilder("a");
+        for (int i = 0; i < 3; i++)
+            new MyThread(s).start();
+    }
+}
